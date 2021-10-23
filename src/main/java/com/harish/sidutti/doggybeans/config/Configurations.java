4428b5865f02aa52ddc8bd34e2a97acf331576df
Harish Sankaran <sharish.sankaran@gmail.com>
2021-10-23 10:03:38 -0700

    Bind Changes

--- src/main/java/com/harish/sidutti/doggybeans/config/Configurations.java
+++ src/main/java/com/harish/sidutti/doggybeans/config/Configurations.java
@@ -7,14 +7,18 @@ import akka.http.javadsl.server.Route;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
 import akka.management.javadsl.AkkaManagement;
 import akka.stream.Materializer;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Locale;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
 
 import static akka.http.javadsl.server.Directives.*;
 import static com.harish.sidutti.doggybeans.config.SpringExtension.SPRING_EXTENSION_PROVIDER;
@@ -24,8 +28,13 @@ public class Configurations {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean
-    public ActorSystem actorSystem(ApplicationContext applicationContext) throws ExecutionException, InterruptedException {
-        ActorSystem system = ActorSystem.create("doggybeans");
+    public ActorSystem actorSystem(ApplicationContext applicationContext) throws UnknownHostException {
+        Config baseConfig = ConfigFactory.load();
+        String serverName = InetAddress.getLocalHost().getCanonicalHostName().toLowerCase(Locale.ROOT);
+        String tcp = "akka.remote.artery.canonical.hostname = " + serverName;
+        log.info("Initializing Config with this={}", tcp);
+        Config remoteConfig = ConfigFactory.parseString(tcp);
+        ActorSystem system = ActorSystem.create("doggybeans", remoteConfig.withFallback(baseConfig));
         SPRING_EXTENSION_PROVIDER.get(system).initialize(applicationContext);
         AkkaManagement.get(system).start();
         log.info("actor system and management initiated");
