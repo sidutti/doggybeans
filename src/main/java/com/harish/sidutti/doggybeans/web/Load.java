0e61b1f4a723605eb57a21255714971089122c71
sidutti <sharish.sankaran@gmail.com>
2021-11-04 17:39:52 -0700

    FileLoad

--- src/main/java/com/harish/sidutti/doggybeans/web/Load.java
+++ src/main/java/com/harish/sidutti/doggybeans/web/Load.java
@@ -1,22 +1,24 @@
 package com.harish.sidutti.doggybeans.web;
 
 import akka.actor.ActorRef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("load")
 public class Load {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(Load.class);
     private final ActorRef metadataActorRef;
     private final ActorRef historyProcessActorRef;
 
@@ -25,13 +27,17 @@ public class Load {
         this.historyProcessActorRef = historyProcessActorRef;
     }
 
-    @GetMapping(value = "nyse")
-    public List<String> loadNYSE() throws IOException {
-        Resource resource = new ClassPathResource("NYSE.txt");
-        Path nyseData = resource.getFile().toPath();
-        return Files.lines(nyseData)
-                .peek(str -> metadataActorRef.tell(str, null))
-                .peek(str -> historyProcessActorRef.tell(str, null))
-                .collect(Collectors.toList());
+    @GetMapping(value = "file/{fileName:.+}")
+    public List<String> loadNYSE(@PathVariable String fileName) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ClassPathResource(fileName).getInputStream()))) {
+            return reader.lines()
+                    .peek(str -> metadataActorRef.tell(str, null))
+                    .peek(str -> historyProcessActorRef.tell(str, null))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return List.of(e.getMessage());
+        }
     }
 }
